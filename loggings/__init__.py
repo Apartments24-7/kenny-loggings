4facf3d44c27cd5ba2e2d632baecc71ec05c676e
Christopher Lien <cjlien42@gmail.com>
2024-10-21 16:38:48 -0500

    Update version number

--- loggings/__init__.py
+++ loggings/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'loggings'
-__version__ = '1.0.0'
+__version__ = '1.1.1'
 __author__ = 'Chris Jones and Kenneth Love'
 __license__ = 'BSD 3-clause'
 __copyright__ = 'Copyright 2013 Chris Jones and Kenneth Love'
